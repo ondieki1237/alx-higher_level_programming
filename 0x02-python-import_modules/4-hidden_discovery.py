7c46795cd7c9039c57e358426deb5b547bd2e65a
ondieki1237 <makoriseth1237@gmail.com>
2022-12-01 20:23:22 +0300

    doing final changes

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
-print()
-print()
 
 
 def main():
     for i in dir(hidden_4):
         if i[0:2] != "__":
             print(i)
+
 
 if __name__ == "__main__":
     main()
