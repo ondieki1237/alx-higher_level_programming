a29fc79c4c2cd8b20f38d07c693659d0a3d16368
ondieki1237 <makoriseth1237@gmail.com>
2022-12-01 13:52:41 +0300

    my files

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-
-
 def main():
     a = 10
     b = 5
@@ -9,6 +7,5 @@ def main():
     op = {0: add, 1: sub, 2: mul, 3: div}
     for i in op:
         print('{:d} {:} {:d} = {:d}'.format(a, sym[i], b, op[i](a, b)))
-
 if __name__ == "__main__":
     main()
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
-
 def main():
     l = len(sys.argv)
     print('{:d} argument{:}'.format(l - 1, '.' if l == 1 else
@@ -9,6 +8,5 @@ def main():
     for arg in sys.argv[1:]:
         print("{:d}: {}".format(i, arg))
         i += 1
-
 if __name__ == "__main__":
     main()
