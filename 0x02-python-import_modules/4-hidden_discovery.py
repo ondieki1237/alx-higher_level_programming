5ae20bdd0f31bdddb169a752a1cb6293791ed8cf
ondieki1237 <makoriseth1237@gmail.com>
2022-12-01 20:30:48 +0300

    finally

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def main():
-    for i in dir(hidden_4):
-        if i[0:2] != "__":
-            print(i)
-
+# 4-hidden_discovery.py
+# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
-    main()
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
