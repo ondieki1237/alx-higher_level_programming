fa312342eeb77c1b43f9385123b537e754b206cb
ondieki1237 <makoriseth1237@gmail.com>
2022-12-01 21:25:22 +0300

    makori

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -6,10 +6,6 @@ def main():
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-<<<<<<< HEAD
 
-
-=======
->>>>>>> b70f8516df12b35d671741e095d5fa5824d7927e
 if __name__ == "__main__":
     main()
