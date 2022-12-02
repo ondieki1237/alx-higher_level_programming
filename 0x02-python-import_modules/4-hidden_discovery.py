945970a2a0a38cc60186f39c41a0b5e5aa49c782
ondieki1237 <makoriseth1237@gmail.com>
2022-12-01 18:25:45 -0800

    the prize is near

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-# 4-hidden_discovery.py
-# Brennan D Baraban <375@holbertonschool.com>
+from variable_load_5 import a
+
+
+def main():
+    print("{:d}".format(a))
+
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    main()
