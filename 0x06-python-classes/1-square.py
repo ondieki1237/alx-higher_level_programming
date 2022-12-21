386b2b006eed491c027b30519df5353bdf193aab
ondieki1237 <makoriseth1237@gmail.com>
2022-12-21 03:55:37 -0800

    a square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
-"""Square Module"""
+""" Square Module """
+
+
 class Square:
-    """This is a Square Class"""
+    """ This is a Square Class """
     def __init__(self, size):
         """ defining the size of square """
         self.__size = size
