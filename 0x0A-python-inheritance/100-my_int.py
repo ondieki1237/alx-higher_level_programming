a6913ef7681258768c3b4c842eadf9f6c321a5f2
ondieki1237 <makoriseth1237@gmail.com>
2023-01-10 21:06:14 -0800

    finally

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+
+
+#!/usr/bin/python3
+class MyInt(int):
+    """class to override int comparrisons"""
+    def __eq__(self, other):
+        return int(self) != int(other)
+
+    def __ne__(self, other):
+        return int(self) == int(other)
