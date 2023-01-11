11d0769f326a18eb35fe752562cfc41366125164
ondieki1237 <makoriseth1237@gmail.com>
2023-01-10 20:49:24 -0800

    making the squares count

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -11,3 +11,10 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__height = height
         self.__width = width
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return "[{}] {:d}/{:d}".format(type(self).__name__, self.__width,
+                                       self.__height)
