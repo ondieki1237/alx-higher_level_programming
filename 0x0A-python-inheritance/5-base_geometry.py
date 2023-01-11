7bc274e8f03e1546186c98460861240e063af613
ondieki1237 <makoriseth1237@gmail.com>
2023-01-10 19:29:13 -0800

    geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+"""module evaluate geometry using maths operations"""
+
+
+class BaseGeometry:
+    """Geometry class with maths operations"""
+    def __init__(self):
+        pass
