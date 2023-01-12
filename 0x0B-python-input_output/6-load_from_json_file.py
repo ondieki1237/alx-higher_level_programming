da3e340c522d2d153c2041f9017ce6c84e931558
ondieki1237 <makoriseth1237@gmail.com>
2023-01-11 20:07:03 -0800

    student to disk reload

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,9 @@
-
-
 #!/usr/bin/python3
+"""Defines a JSON file-reading function."""
 import json
 
 
-def from_json_string(my_str):
-    """loads (decodes/ converts) string to JSON"""
-    return (json.loads(my_str))
-
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
