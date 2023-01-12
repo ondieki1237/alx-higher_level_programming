5a492086b91f6b93d3789f871aabccd9d7301a9c
ondieki1237 <makoriseth1237@gmail.com>
2023-01-11 19:49:02 -0800

    making changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
 def read_file(filename=""):
-    """reads text file"""
-    with open(filename, mode='r', encoding='utf-8') as a_file:
-        print(a_file.read(), end='')
-        a_file.close()
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
