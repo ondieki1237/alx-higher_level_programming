255b5431c036eb14b5a21c6dbe481215f7cf86cb
ondieki1237 <makoriseth1237@gmail.com>
2024-07-30 07:16:47 -0700

    makori

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-
 const fs = require('fs');
+const filePath = process.argv[2];
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
-	  if (err) {
-		      console.log(err);
-		    }
+fs.readFile(filePath, 'utf8', (err, data) => {
+	    if (err) {
+		            console.error(err);
+		            return;
+		        }
+	    console.log(data);
 });
