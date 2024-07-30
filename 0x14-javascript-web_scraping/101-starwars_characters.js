882791c3ba3bc31359c40131df0577c4292c776b
ondieki1237 <makoriseth1237@gmail.com>
2024-07-30 05:49:07 -0700

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,35 @@
-#!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-	  if (!error) {
-		      const characters = JSON.parse(body).characters;
-		      printCharacters(characters, 0);
-		    }
+
+const movieId = process.argv[2];
+if (!movieId) {
+	    console.error('Please provide a Movie ID');
+	    process.exit(1);
+}
+
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(filmUrl, { json: true }, (err, res, body) => {
+	    if (err) {
+		            return console.error('Error:', err);
+		        }
+
+	    if (res.statusCode !== 200) {
+		            return console.error(`Failed to fetch film with ID ${movieId}:`, res.statusCode);
+		        }
+
+	    const characters = body.characters;
+	    characters.forEach(characterUrl => {
+		            request(characterUrl, { json: true }, (charErr, charRes, charBody) => {
+				                if (charErr) {
+							                return console.error('Error:', charErr);
+							            }
+
+				                if (charRes.statusCode !== 200) {
+							                return console.error(`Failed to fetch character:`, charRes.statusCode);
+							            }
+
+				                console.log(charBody.name);
+				            });
+		        });
 });
 
-function printCharacters (characters, index) {
-	  request(characters[index], function (error, response, body) {
-		      if (!error) {
-			            console.log(JSON.parse(body).name);
-			            if (index + 1 < characters.length) {
-					            printCharacters(characters, index + 1);
-					          }
-			          }
-		    });
-}
