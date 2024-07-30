cf7203545d828ee10c2249d95df2b3a0ef75c68d
ondieki1237 <makoriseth1237@gmail.com>
2024-07-30 04:36:42 -0700

    node js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,5 @@
+#!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf8', (err) => {
-	    if (err) {
-		            console.error(err);
-		            return;
-		        }
-	    console.log("The file has been saved!");
+fs.writeFile(process.argv[2], process.argv[3], error => {
+	  if (error) console.log(error);
 });
-
