fbb5d946d0bd58541e9ffeb48d6aef5329ed21e5
ondieki1237 <makoriseth1237@gmail.com>
2024-07-30 04:19:19 -0700

    makori

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf8', (err) => {
+	    if (err) {
+		            console.error(err);
+		            return;
+		        }
+	    console.log("The file has been saved!");
+});
+
